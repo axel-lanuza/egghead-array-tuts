f4f19fa0e5cddf6ba3adc01965568d4e15afb51a
Benjamin Gandhi-Shepard <benjamin@gandhishepard.com>
2015-11-01 10:59:05 -0600

    set up yargs and todo tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,18 +7,27 @@ import once from 'once'
 import openURL from 'opn'
 import watchify from 'watchify'
 import babelify from 'babelify'
+import filesystem from 'fs'
+import todo from 'gulp-todo'
 
-const browser = 'google chrome'
+const browser = 'google chrome canary'
 const argv = yargs
-    .usage('Usage: -f [num]')
+    .usage('Usage: -d [string] -f [num]')
     .argv
 
-const docs = [
-  '01-index.js',
-  '02-another-file.js'
-]
 
-const entry = `./${docs[argv.f - 1]}`
+// http://stackoverflow.com/questions/20822273/best-way-to-get-folder-and-file-list-in-javascript
+var getAllFilesFromFolder = function(dir = 'js', num = 1) {
+  var results = []
+  filesystem.readdirSync(`./${dir}`).forEach(function(file) {
+    results.push(`./${dir}/${file}`)
+  })
+  // Since the directory has a .DS_store file, we don't neet to reduce 'num' by 1. coolish.
+  return results[(num)]
+}
+
+
+const entry = getAllFilesFromFolder(argv.d, argv.f)
 
 //the development task
 gulp.task('default', function(cb) {
@@ -43,6 +52,20 @@ gulp.task('default', function(cb) {
   .once('update', function() {
     ready()
   })
+})
+
+
+//////////
+// TODO //
+//////////
+
+gulp.task('todo', () => {
+ gulp.src(['./js', './src/**/*.{jade,styl,js,yaml}', './gulpfile.babel.js'])
+   .pipe(todo({
+     customTags: ['JS-TODO', 'JADE-TODO', 'CSS-TODO', 'YAML-TODO', 'GULP-TODO']
+   }))
+   .pipe(gulp.dest('.'))
+   // -> Will output a TODO.md with your todos
 })
 
 // Just a task for Console loggin stuff
